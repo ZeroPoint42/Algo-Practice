2d22d0d0c36e866a3a36d29dc33a9edf003a5bf2
ZeroPoint42 <brock.wilmer@yahoo.com>
2023-10-28 10:17:03 -0700

    Update and rename cw-p1 to cw-p1.js

--- CodeWars/cw-p1.js
+++ CodeWars/cw-p1.js
@@ -1,11 +1,13 @@
-// CodeWars problem:
+// CodeWars problem1:
 
 // Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
 
 // The binary number returned should be a string.
 
-// Answer:
 function addBinary(a,b){
+  // calc the sum and conver to binary
+  //the toString() method returns a number in binary when the radix is 2. 
+  //The radix is an optional argument that specifies the base, or number system, in which the number should be represented.
   return (a+b).toString(2)
 }
 const result = addBinary(5,4);
